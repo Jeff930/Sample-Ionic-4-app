5907ad6e1267bf9152eb996f50e5b1aa81abef73
Unknown <jefferson30.jdg@gmail.com>
2019-01-14 00:56:47 +0800

    Created a page named Finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'change-ip', loadChildren: './change-ip/change-ip.module#ChangeIPPageModule' },
   { path: 'send', loadChildren: './send/send.module#SendPageModule' },
+  { path: 'finish', loadChildren: './finish/finish.module#FinishPageModule' },
 ];
 
 @NgModule({
